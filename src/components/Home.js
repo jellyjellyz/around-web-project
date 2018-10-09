b64cebceb3c8f5d46e1dfec51502dbec4784b777
Jiayu Zhang <jellyjellychris@gmail.com>
2018-10-09 14:20:29 -0400

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import $ from 'jquery';
-import { Tabs, Button, Spin } from 'antd';
+import { Tabs, Spin } from 'antd';
 import {GEO_OPTIONS, POS_KEY, API_ROOT, AUTH_PREFIX, TOKEN_KEY} from '../constants';
 import { Gallery } from './Gallery';
+import { CreatePostButton } from './CreatePostButton';
 
 
 const TabPane = Tabs.TabPane;
-const operations = <Button>Create New Post</Button>;
+
 
 export class Home extends React.Component {
     state = {
@@ -58,9 +59,19 @@ export class Home extends React.Component {
             return <Spin tip={"Loading geo location ..."}/>;
         } else if (this.state.loadingPosts) {
             return <Spin tip={"Loading posts ..."}/>;
+        } else if (this.state.posts != null && this.state.posts.length > 0) {
+            return <Gallery images={
+                this.state.posts.map( ({ user, message, url }) => ({
+                    user,
+                    src: url,
+                    thumbnail: url,
+                    caption: message,
+                    thumbnailWidth: 400,
+                    thumbnailHeight: 300
+                }))
+            } />;
         } else {
-            return <Gallery images={imageList}/>;
-
+            return null;
         }
     }
 
@@ -69,17 +80,30 @@ export class Home extends React.Component {
         const { latitude, longitude } = JSON.parse(localStorage.getItem(POS_KEY));
         const token = localStorage.getItem(TOKEN_KEY);
         $.ajax({
-            url: `${API_ROOT}/search?lat=${latitude}&lon=${longitude}&range=20`,
+            url: `${API_ROOT}/search?lat=${latitude}&lon=${longitude}&range=2000`,
             headers: {
                 Authorization: `${AUTH_PREFIX} ${token}`
             }
-        })
+        }).then(
+            (response) => {
+                this.setState({posts: response, loadingPosts: false});
+                console.log(response);
+            },
+            (response) => {
+                this.setState({error: response.responseText})
+            }
+        ).catch (
+            (error) => {
+                console.log(error)
+            }
+        )
         // `${API_ROOT}/search?lat=??&lon=??&range=20`
     }
 
     render() {
+        const createPostButton = <CreatePostButton loadNearbyPosts={this.loadNearbyPosts}/>;
         return (
-            <Tabs tabBarExtraContent={operations} className="main-tabs">
+            <Tabs tabBarExtraContent={ createPostButton } className="main-tabs">
                 <TabPane tab="Post" key="1">
                     {this.getGalleryPanelContent()}
                 </TabPane>
@@ -89,67 +113,72 @@ export class Home extends React.Component {
     }
 }
 
-const imageList = [];
-//
-// const imageList = [
-//     {
-//         user: 'jelly',
-//         src: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_b.jpg",
-//         thumbnail: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_n.jpg",
-//         thumbnailWidth: 271,
-//         thumbnailHeight: 320,
-//         caption: "Orange Macro (Tom Eversley - isorepublic.com)"
-//     },
-//     {
-//         user: 'jelly1',
-//         src: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_b.jpg",
-//         thumbnail: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_n.jpg",
-//         thumbnailWidth: 320,
-//         thumbnailHeight: 190,
-//         caption: "286H (gratisography.com)"
-//     },
-//     {
-//         user: 'jelly2',
-//         src: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_b.jpg",
-//         thumbnail: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_n.jpg",
-//         thumbnailWidth: 320,
-//         thumbnailHeight: 148,
-//         caption: "315H (gratisography.com)"
-//     },
-//     {
-//         src: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_b.jpg",
-//         thumbnail: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_n.jpg",
-//         thumbnailWidth: 320,
-//         thumbnailHeight: 213,
-//         caption: "201H (gratisography.com)"
-//     },
-//     {
-//         src: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_b.jpg",
-//         thumbnail: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_n.jpg",
-//         thumbnailWidth: 248,
-//         thumbnailHeight: 320,
-//         caption: "Big Ben (Tom Eversley - isorepublic.com)"
-//     },
-//     {
-//         src: "https://c1.staticflickr.com/9/8785/28687743710_870813dfde_h.jpg",
-//         thumbnail: "https://c1.staticflickr.com/9/8785/28687743710_3580fcb5f0_n.jpg",
-//         thumbnailWidth: 320,
-//         thumbnailHeight: 113,
-//         caption: "Red Zone - Paris (Tom Eversley - isorepublic.com)"
-//     },
-//     {
-//         src: "https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_b.jpg",
-//         thumbnail: "https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_n.jpg",
-//         thumbnailWidth: 313,
-//         thumbnailHeight: 320,
-//         caption: "Wood Glass (Tom Eversley - isorepublic.com)"
-//     },
-//     {
-//         src: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_b.jpg",
-//         thumbnail: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_n.jpg",
-//         thumbnailWidth: 320,
-//         thumbnailHeight: 213,
-//         caption: "Flower Interior Macro (Tom Eversley - isorepublic.com)"
-//     }
-// ];
-//
+
+
+const imageList = [
+    {
+        user: 'jelly',
+        src: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_b.jpg",
+        thumbnail: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_n.jpg",
+        thumbnailWidth: 271,
+        thumbnailHeight: 320,
+        caption: "Orange Macro (Tom Eversley - isorepublic.com)"
+    },
+    {
+        user: 'jelly1',
+        src: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_b.jpg",
+        thumbnail: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_n.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 190,
+        caption: "286H (gratisography.com)"
+    },
+    {
+        user: 'jelly2',
+        src: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_b.jpg",
+        thumbnail: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_n.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 148,
+        caption: "315H (gratisography.com)"
+    },
+    {
+        user: 'jelly2',
+        src: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_b.jpg",
+        thumbnail: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_n.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 213,
+        caption: "201H (gratisography.com)"
+    },
+    {
+        user: 'jelly2',
+        src: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_b.jpg",
+        thumbnail: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_n.jpg",
+        thumbnailWidth: 248,
+        thumbnailHeight: 320,
+        caption: "Big Ben (Tom Eversley - isorepublic.com)"
+    },
+    {
+        user: 'jelly2',
+        src: "https://c1.staticflickr.com/9/8785/28687743710_870813dfde_h.jpg",
+        thumbnail: "https://c1.staticflickr.com/9/8785/28687743710_3580fcb5f0_n.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 113,
+        caption: "Red Zone - Paris (Tom Eversley - isorepublic.com)"
+    },
+    {
+        user: 'jelly2',
+        src: "https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_b.jpg",
+        thumbnail: "https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_n.jpg",
+        thumbnailWidth: 313,
+        thumbnailHeight: 320,
+        caption: "Wood Glass (Tom Eversley - isorepublic.com)"
+    },
+    {
+        user: 'jelly2',
+        src: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_b.jpg",
+        thumbnail: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_n.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 213,
+        caption: "Flower Interior Macro (Tom Eversley - isorepublic.com)"
+    }
+];
+
